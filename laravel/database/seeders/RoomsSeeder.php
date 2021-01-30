4c65e5136807fd90953fecef59da71f7974febda
JoelGutierrezN <al221910321@gmail.com>
2021-01-29 23:09:11 -0600

    Migración y seeder de Rooms

---
+++ laravel/database/seeders/RoomsSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class RoomsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i = 1; $i <= 50; $i++){
+            DB::table('rooms')->insert([
+                'rank' => rand(1, 5),
+                'cost' => rand(250, 600),
+            ]);
+        }
+    }
+}
