627d8eeebf81987da27b3b32e105701b15bc2881
JoelGutierrezN <al221910321@gmail.com>
2021-01-29 23:17:38 -0600

    Migración States

---
+++ laravel/database/migrations/2021_01_30_051014_states.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class States extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('states', function (Blueprint $table) {
+            $table->id();
+            $table->string('key', 2);
+            $table->string('name', 40);
+            $table->string('shortname', 10);
+            $table->unsignedTinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('states');
+    }
+}
