288445148f6a4678e96ced2254c188fdd82f7454
RosaMontserrat <al221911582@gmail.com>
2021-02-19 23:51:08 -0600

    finalizacion base-index

--- laravel/resources/views/layouts/footer.blade.php
+++ laravel/resources/views/layouts/footer.blade.php
@@ -1,2 +1,60 @@
-<footer>
+<footer class="fondo-negro footer">
+    <div class="logo">
+        <img src="img/logo.png" alt="...">
+    </div>
+    <div class="footer-redes">
+        <ul>
+            <div>
+                <li>
+                    <img src="img/logo.png" alt="...">
+                    <p class="text-white">Facebook</p>
+                </li>
+                <li>
+                    <img src="img/logo.png" alt="...">
+                    <p class="text-white">WhatsApp</p>
+                </li>
+            </div>
+            <div>
+                <li>
+                    <img src="img/logo.png" alt="...">
+                    <p class="text-white">Twitter</p>
+                </li>
+                <li>
+                    <img src="img/logo.png" alt="...">
+                    <p class="text-white">Instragram</p>
+                </li>
+            </div>
+        </ul>
+    </div>
+    <div class="footer-contacto">
+        <ul>
+            <div>
+                <li>
+                    <img src="img/logo.png" alt="...">
+                    <p class="text-white">correo@correo.com</p>
+                </li>
+                <li>
+                    <img src="img/logo.png" alt="...">
+                    <p class="text-white">000-000-0000</p>
+                </li>
+            </div>
+            <div>
+                <li>
+                    <img src="img/logo.png" alt="...">
+                    <p class="text-white">000-00-0-0000</p>
+                </li>
+                <li>
+                    <img src="img/logo.png" alt="...">
+                    <p class="text-white">Direccion</p>
+                </li>
+            </div>
+        </ul>
+    </div>
+    <div class="footer-mapa">
+        <p class="text-white">Mapa de Google Maps</p>
+    </div>
+    <div class="footer-legal">
+        <p class="text-white">Terminos y Condiciones</p>
+        <p class="text-white">Todos los Derechos Reservados</p>
+    </div>
 </footer>
