1bd8e77e7fbcf9ff06610d5d96b0809c35e262e1
RosaMontserrat <al221911582@gmail.com>
2021-02-27 12:36:57 -0600

    Meck

---
+++ app/mock/productos.js
@@ -0,0 +1,30 @@
+module.exports = {
+    '/api/productos':
+      data = [
+        {
+          id: 1
+          , categoria: 'Bebidas'
+          , producto: 'Agua 500 ml'
+          , precio_unitario: 10
+          , existencias: 10
+          , fotografia: 'http://placeimg.com/100/100/people'
+        }
+        , {
+          id: 2
+          , categoria: 'Bebidas'
+          , producto: 'Agua 1 l'
+          , precio_unitario: 13
+          , existencias: 20
+          , fotografia: 'http://placeimg.com/100/100/people'
+        }
+        , {
+          id: 3
+          , categoria: 'Bebidas'
+          , producto: 'Refresco cola 600 ml'
+          , precio_unitario: 12
+          , existencias: 14
+          , fotografia: 'http://placeimg.com/100/100/people'
+        }
+      ]
+}
+    
---
+++ app/mock/usuarios.js
@@ -0,0 +1,50 @@
+module.exports = {
+    '/api/usuarios':
+      data = [
+        {
+          id: 1
+          , rol: 'Administrador'
+          , name: 'Benito Pablo'
+          , primer_apellido: 'Juárez'
+          , segundo_apellido: 'García'
+          , sexo:'Masculino'
+          , nacimiento:'1806-03-24'
+          , email:'benito@correo.com'
+          , avatar: 'http://placeimg.com/100/100/people'
+        }
+        , {
+          id: 2
+          , rol: 'Vendedor'
+          , name: 'Antonio'
+          , primer_apellido: 'López'
+          , segundo_apellido: 'de Santa Ana'
+          , sexo:'Masculino'
+          , nacimiento:'1974-02-21'
+          , email:'antonio@correo.com'
+          , avatar: 'http://placeimg.com/100/100/people'
+        }
+        , {
+          id: 3
+          , rol: 'Vendedor'
+          , name: 'Miguel'
+          , primer_apellido: 'Hidalgo'
+          , segundo_apellido: 'y Costilla'
+          , sexo:'Masculino'
+          , nacimiento:'1753-05-08'
+          , email:'miguel@correo.com'
+          , avatar: 'http://placeimg.com/100/100/people'
+        }
+        , {
+          id: 4
+          , rol: 'Comprador'
+          , name: 'Enrique'
+          , primer_apellido: 'Peña'
+          , segundo_apellido: 'Nieto'
+          , sexo:'Masculino'
+          , nacimiento:'1966-07-20'
+          , email:'enrique@correo.com'
+          , avatar: 'http://placeimg.com/100/100/people'
+        }
+      ]
+}
+    
