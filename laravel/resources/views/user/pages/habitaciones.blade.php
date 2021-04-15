d10afdf9531c7ca21dc1f399f5f4c28430c377e5
JoelGutierrezN <al221910321@gmail.com>
2021-04-15 10:12:52 -0500

    correccion rutas user/premium

--- laravel/resources/views/user/pages/habitaciones.blade.php
+++ laravel/resources/views/user/pages/habitaciones.blade.php
@@ -19,7 +19,7 @@
                 <textarea name="resume" id="resume" class="resume" cols="30" rows="10">Habitacion Sencilla con lo Esencial para el Descanso de tu Mascota</textarea>
                 <p class="precio">$150</p>
                 <div>
-                    <a href="{{ route('reservarVU') }}" class="boton-small boton-naranja">Rentar Ahora</a>
+                    <a href="{{ route('reservarVU', ['date' => 1]) }}" class="boton-small boton-naranja">Rentar Ahora</a>
                 </div>
             </div>
         </div>
@@ -34,7 +34,7 @@
                 <textarea name="resume" id="resume" class="resume" cols="30" rows="10">Habitacion Basica con lo Basico para el Descanso y Distraccion de tu Mascota</textarea>
                 <p class="precio">$250</p>
                 <div>
-                    <a href="{{ route('reservarVU') }}" class="boton-small boton-naranja">Rentar Ahora</a>
+                    <a href="{{ route('reservarVU', ['date' => 1]) }}" class="boton-small boton-naranja">Rentar Ahora</a>
                 </div>
             </div>
         </div>
@@ -50,7 +50,7 @@
                 <textarea name="resume" id="resume" class="resume" cols="30" rows="10">Habitacion Grande Esencial para Razas Grandes o Inquietas</textarea>
                 <p class="precio">$300</p>
                 <div>
-                    <a href="{{ route('reservarVU') }}" class="boton-small boton-naranja">Rentar Ahora</a>
+                    <a href="{{ route('reservarVU', ['date' => 1]) }}" class="boton-small boton-naranja">Rentar Ahora</a>
                 </div>
             </div>
         </div>
@@ -67,7 +67,7 @@
                 <textarea name="resume" id="resume" class="resume" cols="30" rows="10">Habitacion Doble, Apta para cualquier Raza, Disponible para 2 a 4 Mascotas</textarea>
                 <p class="precio">$450</p>
                 <div>
-                    <a href="{{ route('reservarVU') }}" class="boton-small boton-naranja">Rentar Ahora</a>
+                    <a href="{{ route('reservarVU', ['date' => 1]) }}" class="boton-small boton-naranja">Rentar Ahora</a>
                 </div>
             </div>
         </div>
@@ -84,7 +84,7 @@
                 <textarea name="resume" id="resume" class="resume" cols="30" rows="10">Habitacion lujosa, con todas las necesidades, ademas de que puede alojar a un maximo de 10 mascotas</textarea>
                 <p class="precio">$600</p>
                 <div>
-                    <a href="{{ route('reservarVU') }}" class="boton-small boton-naranja">Rentar Ahora</a>
+                    <a href="{{ route('reservarVU', ['date' => 1]) }}" class="boton-small boton-naranja">Rentar Ahora</a>
                 </div>
             </div>
         </div>
